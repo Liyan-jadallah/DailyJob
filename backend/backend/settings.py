f3032dfddb25a56a6e3ca381d24a0e366a02cee7
Liyan-jadallah <mhamadlayan5@gmail.com>
2026-08-14 19:01:26 +0300

    Update DATABASES to use dynamic environment variables 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,10 +75,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres.wextqsamwdclyhivjord'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db.wextqsamwdclyhivjord.supabase.co'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'HOST': os.getenv('DB_HOST', 'aws-0-eu-central-1.pooler.supabase.com'),
+        'PORT': os.getenv('DB_PORT', '6543'),
     }
 }
 
